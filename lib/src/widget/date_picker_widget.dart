15c14d007e95a8e9559e705674062184600e133a
Suwatdev007 <69620703+Suwatdev007@users.noreply.github.com>
2021-02-21 14:04:52 +0700

    Update date_picker_widget.dart

--- lib/src/widget/date_picker_widget.dart
+++ lib/src/widget/date_picker_widget.dart
@@ -237,11 +237,7 @@ class _DatePickerWidgetState extends State<DatePickerWidget> {
   void _changeYearSelection(int index) {
     int year = _yearRange.first + index;
     if (_currYear != year) {
-       if(widget.locale.toString()=='DateTimePickerLocale.th'){
-        _currYear = _currYear+543;
-      }else{
         _currYear = year;
-      }
       _changeDateRange();
       _onSelectedChange();
     }
